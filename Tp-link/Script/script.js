b42f43c29e48401446513e5ee8c1f408de00b892
DavidRodrigue2 <jdavid.rodriguez.e@gmail.com>
2018-09-17 22:53:27 -0500

    Modifica index y se avanca en panel izquierdo de Tplink

--- Tp-link/Script/script.js
+++ Tp-link/Script/script.js
@@ -1,1 +1,24 @@
-boton.addEventListener("click", procesa);
+ol4.addEventListener("click", procesa);
+function procesa(){
+  var a = document.getElementById("ol4").childNodes;
+  var cont = 0;
+  var i = 0;
+  for (i = 0; i < a.length; i++) {
+    if (a[i].className == "hidden") {
+      cont += 1;
+      a[i].className = "show";
+    }
+    else if(a[i].className == "show"){
+      a[i].className = "hidden";
+    }
+  }
+}
+
+function doClick(n)
+{
+  console.log(n);
+  var d = document.getElementById('ol'+n);
+  console.log(d);
+  d.className = "noChildAct";
+  
+}
