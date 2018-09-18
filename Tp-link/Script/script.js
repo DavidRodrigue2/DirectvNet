34d25583d70857325f1531414ce88b5896c6b155
DavidRodrigue2 <jdavid.rodriguez.e@gmail.com>
2018-09-18 00:40:37 -0500

    avance en panel izquierdo de Tplink, despliegue

--- Tp-link/Script/script.js
+++ Tp-link/Script/script.js
@@ -1,24 +1,23 @@
-ol4.addEventListener("click", procesa);
-function procesa(){
-  var a = document.getElementById("ol4").childNodes;
-  var cont = 0;
-  var i = 0;
-  for (i = 0; i < a.length; i++) {
-    if (a[i].className == "hidden") {
-      cont += 1;
-      a[i].className = "show";
-    }
-    else if(a[i].className == "show"){
-      a[i].className = "hidden";
-    }
-  }
-}
-
 function doClick(n)
 {
-  console.log(n);
-  var d = document.getElementById('ol'+n);
-  console.log(d);
-  d.className = "noChildAct";
-  
+  var ab = document.getElementById('ol'+n).childNodes;
+
+  if (ab.length == 1) {
+    d = document.getElementById('a'+n).className = "L2";
+  }
+  else if (ab.length > 1) {
+    d = document.getElementById('ol'+n).className = "withChild";
+
+    var cont = 0;
+    var i = 0;
+    for (i = 0; i < ab.length; i++) {
+      if (ab[i].className == "hidden") {
+        cont += 1;
+        ab[i].className = "show";
+      }
+      else if(ab[i].className == "show"){
+        ab[i].className = "hidden";
+      }
+    }
+  }
 }
