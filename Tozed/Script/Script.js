7c9352ae582ebe5ac442336b2ce6f1f6a2a24c50
DavidRodrigue2 <jdavid.rodriguez.e@gmail.com>
2018-10-17 21:19:31 -0500

    Generando Tozed

--- Tozed/Script/Script.js
+++ Tozed/Script/Script.js
@@ -1,6 +1,8 @@
 const buttonLogin = document.getElementById('buttonLogin');
 const login = document.getElementById('login');
+const loginCenter = document.getElementById('loginCenter');
 
 buttonLogin.addEventListener('click', (event)=>{
   login.classList.add('hidden');
+  loginCenter.classList.add('hidden');
 });
