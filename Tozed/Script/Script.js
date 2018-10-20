d141cdc9207ea1c42954cb9e94e258f2a17161c5
DavidRodrigue2 <jdavid.rodriguez.e@gmail.com>
2018-10-19 20:31:00 -0500

    terminando left post login en Tozed

--- Tozed/Script/Script.js
+++ Tozed/Script/Script.js
@@ -3,8 +3,16 @@ const login = document.getElementById('login');
 const loginCenter = document.getElementById('loginCenter');
 const menu = document.getElementById('menu');
 
+// const link = document.getElementById('a1');
+
 buttonLogin.addEventListener('click', (event)=>{
   login.classList.add('hidden');
   loginCenter.classList.add('hidden');
   menu.classList.remove('hidden');
 });
+
+function doClick(n){
+  alert('ok'+ n);
+  console.log(n);
+  
+}
