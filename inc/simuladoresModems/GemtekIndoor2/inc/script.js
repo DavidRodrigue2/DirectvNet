506d4b36cfbe123e16cdd80fa73a3553c22d3e2e
DavidRodrigue2 <jdavid.rodriguez.e@gmail.com>
2018-11-09 00:03:46 -0500

    Generando intrfaz Gemtek indoor

--- inc/simuladoresModems/GemtekIndoor2/inc/script.js
+++ inc/simuladoresModems/GemtekIndoor2/inc/script.js
@@ -1,16 +1,23 @@
 function doClick(n){
   const nivel1 = document.getElementById('nivel1_'+n);
-  
+  const level2 = document.getElementById('level2_'+n);
+  const icon1 = document.getElementById('icon-left'+n);
   var x = (n * 10) + 1;
-  var z = x + 1;
-  const nivel2 = document.getElementById('nivel2_'+x);
-  const level2 = document.getElementById('level2_'+x);
-  const nivel3 = document.getElementById('nivel3_'+z);
-
-  nivel2.classList.remove('hidden');
-  nivel2.classList.add('select');
-  level2.classList.remove('hidden');
-  level2.classList.add('select');
-  nivel3.classList.remove('hidden');
-  nivel3.classList.add('select');
+  const icon2 = document.getElementById('icon-left'+x);
+  const nivel2 = document.getElementById('nivel2_'+x);  
+  const nivel3 = document.getElementById('nivel3_'+x);
+  switch(n){
+    case 1:
+      level2.classList.toggle('hidden');
+      nivel2.classList.toggle('hidden');
+      nivel3.classList.toggle('hidden');
+      level2.classList.toggle('select');
+      nivel2.classList.toggle('select');
+      nivel3.classList.toggle('select');
+      icon1.classList.toggle('icon-ctrl-down');
+      icon2.classList.toggle('icon-ctrl-down');
+      break;
+    case 2:
+      nivel2.classList.toggle('hidden');
+  }
 }
