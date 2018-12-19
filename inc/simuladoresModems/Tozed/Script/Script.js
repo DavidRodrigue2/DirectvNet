194bc9e32d6fe62fa76ca5859dbc66b6df506fc1
DavidRodrigue2 <jdavid.rodriguez.e@gmail.com>
2018-12-18 22:59:22 -0500

    Modificando estilos en Gemtek indoor y tozed

--- inc/simuladoresModems/Tozed/Script/Script.js
+++ inc/simuladoresModems/Tozed/Script/Script.js
@@ -9,13 +9,6 @@ buttonLogin.addEventListener('click', (event)=>{
   menu.classList.remove('hidden');
 });
 
-const logo2 = document.getElementById('logo2');
-logo2.addEventListener('click', (event)=>{
-  login.classList.remove('hidden');
-  img.src = "Images/center.png";
-  menu.classList.add('hidden');
-});
-
 function doClick(n){
   if (n < 10) {
     const ol = document.getElementById('ol'+n);
@@ -42,32 +35,32 @@ function doClick(n){
       li.classList.add('activLi');
     }
     var r = (n * 10) + 1;
-    center(r);
+    // center(r);
     ol.classList.add('activOl');
     h.classList.add('activH');
   }
   else{
-    center(n);
+    // center(n);
   }
 }
 
-function center(n){
-  if (n == 11){
-    img.src = "Images/center.png";
-  }
-  if (n == 12){
-    img.src = "Images/Version.png";
-  }
-  if (n == 13){
-    img.src = "Images/Dispositivos.png";
-  }
-  if (n == 14){
-    img.src = "Images/Estadisticas.png";
-  }
-  if (n == 15){
-    img.src = "Images/EstadoLTE.png";
-  }  
-  if (n == 21){
-    img.src = "Images/ConfigWiFi.png";
-  }  
-}
+// function center(n){
+//   if (n == 11){
+//     img.src = "Images/center.png";
+//   }
+//   if (n == 12){
+//     img.src = "Images/Version.png";
+//   }
+//   if (n == 13){
+//     img.src = "Images/Dispositivos.png";
+//   }
+//   if (n == 14){
+//     img.src = "Images/Estadisticas.png";
+//   }
+//   if (n == 15){
+//     img.src = "Images/EstadoLTE.png";
+//   }  
+//   if (n == 21){
+//     img.src = "Images/ConfigWiFi.png";
+//   }  
+// }
